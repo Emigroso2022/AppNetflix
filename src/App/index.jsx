a6e3857566062aa3fba7fef50bf2f8263bc7fa71
emiliano <dalupsystems.2021@gmail.com>
2023-02-20 11:20:40 -0300

    Ahora en español

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -4,7 +4,8 @@ import {PeliculasGrid} from "../PeliculasGrid"
 export function App() {
   return (
     <div>
-      <header className='title'>Este es mi Proyesto </header>
+      <header className='title'>Este es mi Proyecto </header>
+      <h1>Alquiler de Peliculas : </h1>
       <main>
         <PeliculasGrid/>
       </main>
